a9d9d3dca0ba272727f5bbdff72170b3a979d943
Auxxxxx <kriukov_2004@mail.ru>
2024-03-12 19:44:03 +0300

    try fix cors

--- app_analytics/main.py
+++ app_analytics/main.py
@@ -14,10 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*s"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*s"]
 )
 
 
