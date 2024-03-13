a2351f561cd654129fe81a91c808d322772c4657
Auxxxxx <kriukov_2004@mail.ru>
2024-03-13 14:36:05 +0300

    fixed cors

--- app_analytics/main.py
+++ app_analytics/main.py
@@ -34,4 +34,4 @@ app.include_router(microcategory.router)
 # t_associations.Base.metadata.create_all(engine)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=80)
